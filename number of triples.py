beef831d74e2045d56af922aca8cb5b1f4dbb454
Zener_085 <57841939+Zener085@users.noreply.github.com>
2020-12-17 00:23:20 +0300

    Delete number of triples.py

--- number of triples.py
+++
@@ -1,8 +0,0 @@
-def number_of_triples():
-    q = 0
-    for x in range(-1000, 1000):
-        for y in range(x, 1000):
-            for z in range(y, 1000):
-                if (x + y + z == 3) and (x ** 3 + y ** 3 + z ** 3 == 24):
-                    q += 1
-    print(q)
